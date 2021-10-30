e10e67539bd1fdf6151e3b239832f59bb1b02f83
Marcus Benrud <marcus.benrud@gmail.com>
2021-10-30 22:04:22 +0200

    added tab_order object and update_tab_order function successfully implemented closing and opening of tabs     - tabs can be opened but not activated, leaving it in italic font         - upon opening another tab, it will replace the other tab           in view     - tabs that are opened and activated are docked to the navbar     - tabs can be closed     - a new tab is focused when an in focus tab is closed

--- responsive_web_design/personal_portfolio_new/script.js
+++ responsive_web_design/personal_portfolio_new/script.js
@@ -34,71 +34,149 @@ let toggle_folder = (node) => {
 
 // TAB SYSTEM
 
-// add a tab to dock, and focus
-const activate_tab = (tab_id) => {
+const tab_order = {
+    get_tab_amount: () => {
+        // how many tabs are open?
+        return document.getElementsByClassName("nav-link-active").length;
+    },
+    get_temp_tab_number: () => {
 
-
-
-    // remove nav-in-focus class from other tabs
-    for (const link of document.getElementsByClassName("nav-link")) {
-        if (link.classList.contains("nav-in-focus")) {
-            link.classList.toggle("nav-in-focus");
+        for (const tab of document.getElementsByClassName("nav-link-active")) {
+            if (tab.classList.contains("nav-link-view")) return parseInt(tab.style.order);
         }
+        return undefined;
     }
-
-    // unfocus other tabs
-    for (const tab of document.getElementsByClassName("tab")) {
-        if (tab.classList.contains("tab-view")) {
-            tab.classList.toggle("tab-view");
-        }
-    }
-
-    const tab = document.getElementById(tab_id);
-    const tab_link = document.getElementById(tab_id+"-link");
-    console.log(tab_id);
-
-    tab_link.classList.toggle("nav-link-active");
-
-    view_tab(tab.id);
-
-    tab_link.classList.toggle("nav-in-focus");
-    tab.classList.toggle("tab-view");
-    tab_link.children[0].style.visibility = "visible";
 }
 
-// unfocus and remove from dock
-const close_tab = (tab_id) => {
-    console.log(tab_id)
-    // reveal background
-    document.getElementById("welcome-inner").style.display = "flex";
-};
+const update_tab_order = () => {
+    let order = 0;
+    for (const link of document.getElementsByClassName("nav-link")) {
+        if(link.classList.contains("nav-link-active")) {
+            link.style.order = order;
+            order++;
+        }
+    }
+}
 
-// view, but do not put in dock
-const view_tab = (tab_id) => {
+// returns a project link, for use in open tabs view
+const project_link = (innerHTML) => {
+    const elem = document.createElement("a");
+    elem.classList.add("project-link");
+    elem.innerHTML = '<img class="file-img" src="image_files/mb_logo.svg"></img>' + innerHTML;
+
+    return elem;
+}
+
+// focus on open tab
+const focus_tab = (tab_id) => {
+
+    // if tab is being closed do not focus
+    if (!document.getElementById(tab_id+"-link").classList.contains("nav-link-active")) return;
+
+    for (const tab of document.getElementsByClassName("tab-view")) {
+        tab.classList.toggle("tab-view");
+    }
+
+    for (const tab_link of document.getElementsByClassName("nav-in-focus")) {
+        tab_link.classList.toggle("nav-in-focus");
+    }
+
+    document.getElementById(tab_id + "-link").classList.toggle("nav-in-focus");
+    document.getElementById(tab_id).classList.toggle("tab-view");
+}
+
+// open a tab
+const open_tab = (tab_id) => {
+
+    if (document.getElementsByClassName("nav-link-active").length == 0) 
+        document.getElementById("welcome-inner").style.display = "none";
+
+    const tab_link = document.getElementById(tab_id + "-link");
+
+    // if already open do nothing
+    if (tab_link.classList.contains("nav-link-active")) return;
+
+    tab_link.classList.toggle("nav-link-active");
+}
+ 
+// close a tab
+const close_tab = (tab_id) => {
     const tab = document.getElementById(tab_id);
     const tab_link = document.getElementById(tab_id + "-link");
 
-    // 1. Check if there is another tab being viewed that is not set to open
-    for (const open_tab of document.getElementsByClassName("nav-link")) {
-        if (open_tab.classList.contains("nav-link-view")) {
-            open_tab.classList.toggle("nav-link-view")
-        }
+    // remove view class from tab if in focus
+    if (tab_link.classList.contains("nav-in-focus"))
+        tab.classList.toggle("tab-view");
+
+    tab_link.classList.toggle("nav-link-active");
+    tab_link.classList.contains("nav-link-view") ? tab_link.classList.toggle("nav-link-view") : null ;
+
+    update_tab_order();
+    // temp next open tab
+    for (const open_tab of document.getElementsByClassName("nav-link-active")) {
+        focus_tab(open_tab.id.slice(0,-5));
+        return;
     }
 
-    if (tab_link.classList.contains("nav-link-active"));
-
-    // if viewing not open - hide that tab, and view this
-    // if no only viewing tabs - open new
-
-
+    // no more tabs: re-render welcome:
+    document.getElementById("welcome-inner").style.display = "flex";
 }
 
-const focus_tab = (tab_id) => {
+// view a tab without docking 
+const view_tab = (tab_id) => {
     
+    const tab_link = document.getElementById(tab_id + "-link");
+
+    // if open - change
+    if (tab_link.classList.contains("nav-link-active")) {
+        focus_tab(tab_id);
+        return;
+    }
+
+    // if already in view - do nothing
+    if (tab_link.classList.contains("nav-link-view")) return;
+
+    if (tab_order.get_temp_tab_number() === undefined) {
+        console.log("push temp tab to the back!");
+        tab_link.style.order = tab_order.get_tab_amount() + 1;
+    } else {
+        console.log("replace temp tab!");
+        tab_link.style.order = tab_order.get_temp_tab_number();
+    }
+
+    // close other temporary open tabs:
+    if (document.getElementsByClassName("nav-link-view").length) {
+        close_tab(
+            document.getElementsByClassName("nav-link-view")[0].id.slice(0,-5)
+        );
+    }
+
+    open_tab(tab_id);
+    focus_tab(tab_id);
+ 
+    tab_link.classList.toggle("nav-link-view");
 }
 
+// add a tab to dock, and focus
+const activate_tab = (tab_id) => {
+    const tab_link = document.getElementById(tab_id + "-link");
+
+    open_tab(tab_id);
+
+    tab_link.style.order = tab_order.get_tab_amount() + 1;
+    document.getElementById("placeholder-open-tabs").style.display = "none";
+    document.getElementById("open-tabs-inner").appendChild(project_link(tab_id));
+
+    focus_tab(tab_id);
 
 
+
+    if (tab_link.classList.contains("nav-link-view")) {
+        tab_link.classList.toggle("nav-link-view");
+    }
+}
+
+// changing visibility of close button
 const toggle_close_tab = (elem)  => {
     elem.children[0].classList.toggle("visible");
 }
@@ -123,15 +201,15 @@ window.onload = () => {
             toggle_close_tab(navlink);
         })
 
+        // close tab when pressing x
         navlink.children[0].addEventListener("click", () => {
             close_tab(navlink.id.slice(0, -5));
         })
 
-        // focus to a tab if 
+        // focus to a tab if link open
         navlink.addEventListener("click", () => {
             if (navlink.classList.contains("nav-in-focus")) return;
-            console.log("focusing!");
-            focus_tab(navlink.textContent);
+            focus_tab(navlink.id.slice(0,-5));
         })
     }
 
@@ -140,12 +218,11 @@ window.onload = () => {
     for (const tablink of document.getElementsByClassName("project-link")) {
         
         tablink.addEventListener("click", () => {
-            console.log("viewing tab");
+            if (tablink.classList.contains("nav-link-view")) return;
             view_tab(tablink.textContent);
         })
         tablink.addEventListener("dblclick", () => {
             if (tablink.classList.contains("nav-link-active")) return;
-            console.log("activating tab");
             activate_tab(tablink.textContent);
         })
     }
